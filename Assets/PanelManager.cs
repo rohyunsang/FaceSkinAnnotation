16bc024d2b00364362dc4c7a6908f1f05c218c21
Roh <82229769+rohyunsang@users.noreply.github.com>
2023-07-12 00:18:12 +0900

    Update PanelManager.cs

--- Assets/PanelManager.cs
+++ Assets/PanelManager.cs
@@ -1,17 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PanelManager : MonoBehaviour
 {
     public GameObject InitPanel;
+    public GameObject OptionPanel;
+
+    public void OnInitPanel()
+    {
+        InitPanel.SetActive(true);
+    }
+
+    public void OnOptionPanel()
+    {
+        OptionPanel.SetActive(true);
+    }
 
     public void OffInitPanel()
     {
         InitPanel.SetActive(false);
     }
-    public void OnInitPanel()
+
+    public void OffOptionPanel()
     {
-        InitPanel.SetActive(true);
+        OptionPanel.SetActive(false);
     }
+    
+
+    
 }
