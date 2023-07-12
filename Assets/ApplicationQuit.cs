f6fc9ed31358aa5241453b749f804d252f8bccbb
Roh <82229769+rohyunsang@users.noreply.github.com>
2023-07-13 01:15:43 +0900

    rename

--- Assets/ApplicationQuit.cs
+++ Assets/ApplicationQuit.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ApplicationQuit : MonoBehaviour
 {
-    public void OnApplicationQuit()
+    public void QuitApplication()
     {
         Application.Quit();
     }
