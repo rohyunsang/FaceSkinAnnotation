111777e03ac618a69c807ab950f1ae337173b760
Roh <82229769+rohyunsang@users.noreply.github.com>
2023-07-27 05:28:25 +0900

    passing status name

--- Assets/BtnManager.cs
+++ Assets/BtnManager.cs
@@ -5,13 +5,13 @@ using UnityEngine.UI;
 
 public class BtnManager : MonoBehaviour  // button 클릭 넘버랑 번호 
 {
+    public GameObject JsonManager;
     public GameObject[] btns;
     public int buttonValue;
     public string statusName = "";
-    public string facePartictionStatusName = "";
-
     void Start()
     {
+        JsonManager.GetComponent<Json>();
         for (int i = 0; i < btns.Length; i++)
         {
             int buttonIndex = i; // 이것은 클로저 문제를 피하기 위한 것입니다.
@@ -22,16 +22,13 @@ public class BtnManager : MonoBehaviour  // button 클릭 넘버랑 번호
     void ButtonClick(int buttonIndex)
     {
         buttonValue = buttonIndex;
-        Debug.Log("Button " + buttonIndex + " clicked. Button value is now: " + buttonValue);
+        Debug.Log(this.statusName + " : " + buttonIndex.ToString());
+
     }
-    public void SetStatusName(string facePartictionStatusName)
-    {
-        this.facePartictionStatusName = facePartictionStatusName;
-        Debug.Log(facePartictionStatusName);
-    }
-    public void SetFacePartictionStatusName(string statusName)
+    public void SetStatusName(string statusName)
     {
         this.statusName = statusName;
         Debug.Log(statusName);
     }
+    
 }
