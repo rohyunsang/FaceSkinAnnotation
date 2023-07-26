c1bac9489a47744e772a58d6987dde0756ca8df9
Roh <82229769+rohyunsang@users.noreply.github.com>
2023-07-27 05:28:34 +0900

    Update PassingPartName.cs

--- Assets/PassingPartName.cs
+++ Assets/PassingPartName.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PassingPartName : MonoBehaviour
+public class PassingPartName : MonoBehaviour  // 현재 영역 이름 json에 전달 후 status 뛰우는용 
 {
     private GameObject JsonManagerObj; // in csharp can not define variable in function
 
 
-    public void ButtonNamePass()
+    public void ButtonNamePass() //prefab에서 참조 
     {
         string objName = transform.name;
         JsonManagerObj = GameObject.Find("JsonManager");
